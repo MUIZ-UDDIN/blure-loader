32133ba84f7120436335a04813c23a0ce10e3f04
mueez-uddin <86313423+mueez-uddin@users.noreply.github.com>
2021-06-22 21:15:44 +0500

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,22 @@
+const loder = document.querySelector('.loder');
+const bg = document.querySelector('.bg-pic');
+
+let load = 0;
+
+let int = setInterval(blurring, 30)
+function blurring() {
+    load++
+
+    if (load > 99) {
+        clearInterval(int)
+    }
+
+    loder.innerText = `${load}%`;
+    loder.style.opacity = scale(load, 0,100,1,0);
+    bg.style.filter = `blur(${scale(load, 0,100,30,0)}px)`;
+}
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return((num - in_min)* (out_max - out_min)) / (in_max - in_min)
+    + out_min  
+}
